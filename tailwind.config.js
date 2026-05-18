223711135b7d5bfe22a0524725c369f02d9ee140
haymankhetwai <haymankhetwai@gmail.com>
2026-05-18 15:33:17 +0630

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+      
+        volkhov: ['Volkhov', 'sans-serif'], 
+      },
+    },
+  },
+  plugins: [],
+};
